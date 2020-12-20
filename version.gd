8d43207393ed97884e589a409f6bbacccf4e64e8
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-20 18:21:21 -0500

    Kill text file

--- version.gd
+++ version.gd
@@ -1,14 +1,4 @@
 extends Node
 
-func _load_text_file(path):
-	var f = File.new()
-	var err = f.open(path, File.READ)
-	if err != OK:
-		printerr("Could not open file, error code ", err)
-		return null
-	var text = f.get_as_text()
-	f.close()
-	return text
-
 func version() -> String:
-	return _load_text_file("res://version.txt")
+	return "0.0.0"
