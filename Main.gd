11d2eea60f19230c0894b2fea8bbfda65f700ab3
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-20 18:57:07 -0500

    Demonstrate reloading pck file
    Squashed commit of the following:
    
    commit 948a90b200e348d03b08849e40a56352978e1c1d
    Author: Terkwood <38859656+Terkwood@users.noreply.github.com>
    Date:   Sun Dec 20 18:56:57 2020 -0500
    
        demonstrate patch
    
    commit 6390f4055e82413521a306abca9c6603a24443f7
    Author: Terkwood <38859656+Terkwood@users.noreply.github.com>
    Date:   Sun Dec 20 18:55:20 2020 -0500
    
        update patch file
    
    commit 289f161135c5ab7cebca617bf9a672dac313cdd5
    Author: Terkwood <38859656+Terkwood@users.noreply.github.com>
    Date:   Sun Dec 20 18:54:05 2020 -0500
    
        trim
    
    commit 6dd544b9c6f25daa85e836535a85fb56482e68b9
    Author: Terkwood <38859656+Terkwood@users.noreply.github.com>
    Date:   Sun Dec 20 18:52:05 2020 -0500
    
        update patch
    
    commit 93fb9cbc295f38d613a767bd7c315a6cb164cafd
    Author: Terkwood <38859656+Terkwood@users.noreply.github.com>
    Date:   Sun Dec 20 18:49:27 2020 -0500
    
        update patch file
    
    commit e3796d1b140a83ea2ac6dfc851874509dd247d38
    Author: Terkwood <38859656+Terkwood@users.noreply.github.com>
    Date:   Sun Dec 20 18:46:57 2020 -0500
    
        hack script
    
    commit fda525ce14a21f105f9bd96d482104962f520dc3
    Author: Terkwood <38859656+Terkwood@users.noreply.github.com>
    Date:   Sun Dec 20 18:42:31 2020 -0500
    
        make example

--- Main.gd
+++ Main.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-func _ready():
-	$"Panel/VBoxContainer/Version Label".text = "Running v%s" % Version.version()
-	
-
 func _on_Timer_timeout():
-	ProjectSettings.load_resource_pack("res://test-0.0.0-a.pck")
+	ProjectSettings.load_resource_pack("res://test-0.0.0-PATCHED.pck")
 	get_tree().change_scene("res://Main.tscn")
--- version.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-func version() -> String:
-	return "0.0.0"
