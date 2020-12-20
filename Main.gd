4126847e5fd67b0f3778e4a04beebda6e718bccc
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-20 18:38:05 -0500

    stub out example

--- Main.gd
+++ Main.gd
@@ -2,3 +2,8 @@ extends Node2D
 
 func _ready():
 	$"Panel/VBoxContainer/Version Label".text = "Running v%s" % Version.version()
+	
+
+func _on_Timer_timeout():
+	ProjectSettings.load_resource_pack("res://test-0.0.0-a.pck")
+	get_tree().change_scene("res://Main.tscn")
