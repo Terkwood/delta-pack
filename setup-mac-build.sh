14155f7a95aed0131163d9e7167665f645511b50
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-21 18:35:25 -0500

    update script

--- setup-mac-build.sh
+++ setup-mac-build.sh
@@ -6,5 +6,6 @@ brew install FiloSottile/musl-cross/musl-cross
 # see https://github.com/imageworks/OpenShadingLanguage/issues/1055
 # works around an issue with wchar.h not found
 xcode-select --install
+softwareupdate --all --install --force
 open /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg
 brew reinstall gcc
