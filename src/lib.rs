9a47a6ca787cda4b2261786345276a452c00a6c1
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-21 17:59:37 -0500

    stub out rust project

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use gdnative::api::*;
+use gdnative::prelude::*;
+
+#[derive(gdnative::NativeClass)]
+#[inherit(Label)]
+struct IncrementalPatch {
+    bogus: u32,
+}
+
+#[gdnative::methods]
+impl IncrementalPatch {
+    fn new(_owner: &Label) -> Self {
+        IncrementalPatch { bogus: 0 }
+    }
+
+    #[export]
+    fn _ready(&mut self, _owner: &Label) {
+        godot_print!("Hello from rust")
     }
 }
+
+fn init(handle: InitHandle) {
+    handle.add_class::<IncrementalPatch>();
+}
+
+godot_init!(init);
