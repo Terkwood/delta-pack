1130bd3107d7c529b4bb19ae57a06d8b056f4736
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-21 18:02:04 -0500

    Add dummy bindings to rust (#3)

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use gdnative::api::*;
+use gdnative::prelude::*;
+
+#[derive(gdnative::NativeClass)]
+#[inherit(Label)]
+struct IncrementalPatch {
+    bogus: u32,
+}
+
+#[gdnative::methods]
+impl IncrementalPatch {
+    fn new(_owner: &Label) -> Self {
+        IncrementalPatch { bogus: 0 }
+    }
+
+    #[export]
+    fn _ready(&mut self, _owner: &Label) {
+        godot_print!("Hello from rust with bogus value: {}", self.bogus)
+    }
+}
+
+fn init(handle: InitHandle) {
+    handle.add_class::<IncrementalPatch>();
+}
+
+godot_init!(init);
