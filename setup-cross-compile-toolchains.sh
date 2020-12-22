38fb6a0d23ac419aba832e5d91c9aae70da9a8f0
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-21 19:19:10 -0500

    trim

--- setup-cross-compile-toolchains.sh
+++ setup-cross-compile-toolchains.sh
@@ -3,5 +3,4 @@
 # https://timryan.org/2018/07/27/cross-compiling-linux-binaries-from-macos.html
 
 rustup target add x86_64-unknown-linux-musl
-#rustup target add x86_64-unknown-linux-gnu
 rustup target add x86_64-apple-darwin
