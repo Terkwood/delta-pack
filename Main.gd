c5e5d6e0a866b1cdbfdd71313359002b07814607
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-22 18:46:37 -0500

    hack in patch after HTTP get of diff payload

--- Main.gd
+++ Main.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
+const _PATCH_NAME = "test-0.0.0_to_test-0.0.0-DELTA.bin"
+
 func _on_Timer_timeout():
 	ProjectSettings.load_resource_pack("res://test-0.0.0-PATCHED.pck")
 	get_tree().change_scene("res://Main.tscn")
 
 func _ready():
-	var patch_status = get_node_or_null("CenterContainer/VBoxContainer/Patch Status")
-	if patch_status:
-		patch_status.test_patch()
+	
 	
 	var metadata_request = get_node_or_null("MetadataRequest")
 	if metadata_request:
@@ -15,7 +15,7 @@ func _ready():
 	
 	var delta_bin_request = get_node_or_null("DeltaBinRequest")
 	if delta_bin_request:
-		delta_bin_request.request("http://127.0.0.1:8080/test-0.0.0_to_test-0.0.0-DELTA.bin")
+		delta_bin_request.request("http://127.0.0.1:8080/%s" % _PATCH_NAME)
 
 
 func _on_MetadataRequest_request_completed(result, response_code, headers, body):
@@ -25,6 +25,13 @@ func _on_MetadataRequest_request_completed(result, response_code, headers, body)
 
 func _on_DeltaBinRequest_request_completed(result, response_code, headers, body):
 	if response_code == 200:
+		var file = File.new()
+		file.open("user://%s" % _PATCH_NAME, File.WRITE)
+		file.store_buffer(body)
+		file.close()
 		
+		var patch_status = get_node_or_null("CenterContainer/VBoxContainer/Patch Status")
+		if patch_status:
+			patch_status.test_patch()
 	else:
 		printerr("Bad response to delta bin request: %d" % response_code)
