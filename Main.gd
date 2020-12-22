11501474537fed8f8ddd6aa31f3d3c46b5bf6335
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-22 18:19:15 -0500

    add an example of HTTP GET

--- Main.gd
+++ Main.gd
@@ -8,3 +8,11 @@ func _ready():
 	var patch_status = get_node_or_null("CenterContainer/VBoxContainer/Patch Status")
 	if patch_status:
 		patch_status.test_patch()
+	
+	var http_request = get_node_or_null("HTTPRequest")
+	if http_request:
+		http_request.request("https://jsonplaceholder.typicode.com/todos/1")
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	print("HTTP result: %s" % json.result)
