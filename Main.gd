40d6d11aafa9cd47ebf4023045138d2528d44f2c
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-22 15:33:13 -0500

    Demonstrates applying a patch in Godot (#11)

--- Main.gd
+++ Main.gd
@@ -3,3 +3,8 @@ extends Node2D
 func _on_Timer_timeout():
 	ProjectSettings.load_resource_pack("res://test-0.0.0-PATCHED.pck")
 	get_tree().change_scene("res://Main.tscn")
+
+func _ready():
+	var patch_status = get_node_or_null("CenterContainer/VBoxContainer/Patch Status")
+	if patch_status:
+		patch_status.test_patch()
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,68 @@
+use anyhow::{Context, Result};
 use gdnative::api::*;
 use gdnative::prelude::*;
+use std::{
+    fs::File,
+    io::{self, BufReader, BufWriter},
+    path::PathBuf,
+    str::FromStr,
+    time::Instant,
+};
 
 #[derive(gdnative::NativeClass)]
 #[inherit(Label)]
-struct IncrementalPatch {
-    bogus: u32,
-}
+struct IncrementalPatch;
 
+const OLD_PCK: &str = "test-0.0.0.pck";
+const DIFF_BIN: &str = "test-0.0.0_to_test-0.0.0-DELTA.bin";
+const OUT_PCK: &str = "test-0.0.0-DELTA.pck";
+
+/// Apply a patch, as in https://github.com/divvun/bidiff/blob/1e6571e8f36bba3292b33a4b7dfe4ce93a3abd1e/crates/bic/src/main.rs#L257
 #[gdnative::methods]
 impl IncrementalPatch {
     fn new(_owner: &Label) -> Self {
-        IncrementalPatch { bogus: 0 }
+        IncrementalPatch  
+    }
+
+    #[export]
+    fn test_patch(&self, _owner: &Label) {
+        patch(
+            &PathBuf::from_str(OLD_PCK).expect("path to old PCK"),
+            &PathBuf::from_str(DIFF_BIN).expect("path to diff bin"),
+            &PathBuf::from_str(OUT_PCK).expect("path to output PCK"),
+        )
+        .expect("boom")
     }
 
     #[export]
     fn _ready(&mut self, _owner: &Label) {
-        godot_print!("Hello from rust with bogus value: {}", self.bogus)
+        godot_print!("Delta Pack ready");
     }
+}
+
+fn patch(older: &PathBuf, patch: &PathBuf, output: &PathBuf) -> Result<()> {
+    let start = Instant::now();
+
+    let compatch_r = BufReader::new(File::open(patch).context("open patch file")?);
+    let (patch_r, patch_w) = pipe::pipe();
+    use comde::Decompressor;
+    let zstd_decompress = comde::zstd::ZstdDecompressor::new();
+
+    std::thread::spawn(move || {
+        zstd_decompress
+            .copy(compatch_r, patch_w)
+            .context("decompress")
+            .unwrap();
+    });
+
+    let older_r = File::open(older)?;
+    let mut fresh_r = bipatch::Reader::new(patch_r, older_r).context("read patch")?;
+    let mut output_w = BufWriter::new(File::create(output).context("create patch file")?);
+    io::copy(&mut fresh_r, &mut output_w).context("write output file")?;
+
+    godot_print!("Patch applied in {:?}", start.elapsed());
+
+    Ok(())
 }
 
 fn init(handle: InitHandle) {
