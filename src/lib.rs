4c6d5d6a01a7998c7430c1f32d5870003ea982d4
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-22 13:07:44 -0500

    stub out godot/rust link for applying a patch

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,20 @@
+use anyhow::{Context, Result};
 use gdnative::api::*;
 use gdnative::prelude::*;
+use std::{
+    fs::File,
+    io::{self, BufReader, BufWriter},
+    path::PathBuf, 
+    time::Instant,
+};
 
 #[derive(gdnative::NativeClass)]
 #[inherit(Label)]
 struct IncrementalPatch {
     bogus: u32,
 }
-
+ 
+/// Apply a patch, as in https://github.com/divvun/bidiff/blob/1e6571e8f36bba3292b33a4b7dfe4ce93a3abd1e/crates/bic/src/main.rs#L257
 #[gdnative::methods]
 impl IncrementalPatch {
     fn new(_owner: &Label) -> Self {
@@ -14,9 +22,39 @@ impl IncrementalPatch {
     }
 
     #[export]
+    fn test_patch(&self, _owner: &Label) {
+        apply_patch(todo!(), todo!(), todo!()).expect("boom")
+    }
+
+    #[export]
     fn _ready(&mut self, _owner: &Label) {
         godot_print!("Hello from rust with bogus value: {}", self.bogus)
     }
+}
+
+fn apply_patch(older: &PathBuf, patch: &PathBuf, output: &PathBuf) -> Result<()> {
+    let start = Instant::now();
+
+    let compatch_r = BufReader::new(File::open(patch).context("open patch file")?);
+    let (patch_r, patch_w) = pipe::pipe();
+    use comde::Decompressor;
+    let zstd_decompress = comde::zstd::ZstdDecompressor::new();
+
+    std::thread::spawn(move || {
+        zstd_decompress
+            .copy(compatch_r, patch_w)
+            .context("decompress")
+            .unwrap();
+    });
+
+    let older_r = File::open(older)?;
+    let mut fresh_r = bipatch::Reader::new(patch_r, older_r).context("read patch")?;
+    let mut output_w = BufWriter::new(File::create(output).context("create patch file")?);
+    io::copy(&mut fresh_r, &mut output_w).context("write output file")?;
+
+    godot_print!("Patch applied in {:?}", start.elapsed());
+
+    Ok(())
 }
 
 fn init(handle: InitHandle) {
