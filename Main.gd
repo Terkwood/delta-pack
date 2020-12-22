44b7dd5b6a7338e9183994e6c0ee94005c905222
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-22 15:26:53 -0500

    clean up example

--- Main.gd
+++ Main.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-onready var _patch_status = $"CenterContainer/VBoxContainer/Patch Status"
-
 func _on_Timer_timeout():
 	ProjectSettings.load_resource_pack("res://test-0.0.0-PATCHED.pck")
 	get_tree().change_scene("res://Main.tscn")
 
 func _ready():
-	_patch_status.test_patch()
+	var patch_status = get_node_or_null("CenterContainer/VBoxContainer/Patch Status")
+	if patch_status:
+		patch_status.test_patch()
