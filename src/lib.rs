91311446ac59442d131a8e67a5ef2defbd87e459
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-22 15:16:49 -0500

    write paths

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,8 @@ use gdnative::prelude::*;
 use std::{
     fs::File,
     io::{self, BufReader, BufWriter},
-    path::PathBuf, 
+    path::PathBuf,
+    str::FromStr,
     time::Instant,
 };
 
@@ -13,7 +14,11 @@ use std::{
 struct IncrementalPatch {
     bogus: u32,
 }
- 
+
+const OLD_PCK: &str = "test-0.0.0.pck";
+const DIFF_BIN: &str = "test-0.0.0_to_test-0.0.0-DELTA.bin";
+const OUT_PCK: &str = "test-0.0.0-DELTA.pck";
+
 /// Apply a patch, as in https://github.com/divvun/bidiff/blob/1e6571e8f36bba3292b33a4b7dfe4ce93a3abd1e/crates/bic/src/main.rs#L257
 #[gdnative::methods]
 impl IncrementalPatch {
@@ -23,7 +28,12 @@ impl IncrementalPatch {
 
     #[export]
     fn test_patch(&self, _owner: &Label) {
-        apply_patch(todo!(), todo!(), todo!()).expect("boom")
+        patch(
+            &PathBuf::from_str(OLD_PCK).expect("path to old PCK"),
+            &PathBuf::from_str(DIFF_BIN).expect("path to diff bin"),
+            &PathBuf::from_str(OUT_PCK).expect("path to output PCK"),
+        )
+        .expect("boom")
     }
 
     #[export]
@@ -32,7 +42,7 @@ impl IncrementalPatch {
     }
 }
 
-fn apply_patch(older: &PathBuf, patch: &PathBuf, output: &PathBuf) -> Result<()> {
+fn patch(older: &PathBuf, patch: &PathBuf, output: &PathBuf) -> Result<()> {
     let start = Instant::now();
 
     let compatch_r = BufReader::new(File::open(patch).context("open patch file")?);
