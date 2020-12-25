77640a4d0029adad4cd226e2be100158c0105e46
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-25 14:08:35 -0500

    add semver regex

--- write-delta/src/main.rs
+++ write-delta/src/main.rs
@@ -6,10 +6,16 @@ use structopt::StructOpt;
 use validator::Validate;
 lazy_static! {
     static ref RE_HEX: Regex = Regex::new(r"^[0-9a-fA-F]+$").unwrap();
+    /// See https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+    static ref RE_SEMVER: Regex = Regex::new(r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$").unwrap();
 }
 #[derive(StructOpt, Debug, Validate)]
 #[structopt(name = "metadata-writer")]
 struct Delta {
+    #[validate(regex = "RE_SEMVER")]
+    #[structopt(short, long)]
+    release_version: String,
+
     /// URL for the diff binary
     #[validate(url)]
     #[structopt(long)]
