4b09e7a996b0c5ec8cdda47b94f1d649f40300b1
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-25 14:35:08 -0500

    finish up

--- write-delta/src/main.rs
+++ write-delta/src/main.rs
@@ -3,6 +3,8 @@ extern crate lazy_static;
 #[macro_use]
 extern crate serde_derive;
 
+use std::path::PathBuf;
+
 use regex::Regex;
 use sled::IVec;
 use structopt::StructOpt;
@@ -13,9 +15,13 @@ lazy_static! {
     /// See https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
     static ref RE_SEMVER: Regex = Regex::new(r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$").unwrap();
 }
-#[derive(StructOpt, Debug, Validate, Serialize, Deserialize)]
+#[derive(StructOpt, Debug, Validate)]
 #[structopt(name = "write-delta")]
-struct Delta {
+struct Opts {
+    /// Path to the directory for the embedded DB data
+    #[structopt(long, parse(from_os_str))]
+    data_dir: PathBuf,
+
     /// The current version of the PCK file
     #[validate(regex = "RE_SEMVER")]
     #[structopt(short, long)]
@@ -43,13 +49,48 @@ struct Delta {
     expected_pck_b2bsum: String,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct Delta {
+    pub id: u64,
+    pub release_version: String,
+    pub previous_version: String,
+    pub diff_url: String,
+    pub diff_b2bsum: String,
+    pub expected_pck_b2bsum: String,
+}
+
+impl From<(u64, Opts)> for Delta {
+    fn from(
+        (
+            id,
+            Opts {
+                diff_url,
+                diff_b2bsum,
+                expected_pck_b2bsum,
+                release_version,
+                previous_version,
+                data_dir: _,
+            },
+        ): (u64, Opts),
+    ) -> Self {
+        Delta {
+            id,
+            diff_b2bsum,
+            diff_url,
+            expected_pck_b2bsum,
+            release_version,
+            previous_version,
+        }
+    }
+}
+
 fn main() -> sled::Result<()> {
-    let delta = Delta::from_args();
-    if let Err(e) = delta.validate() {
+    let opts = Opts::from_args();
+    if let Err(e) = opts.validate() {
         eprintln!("Failed to validate input: {}", e);
         std::process::exit(1)
     }
-    println!("Writing delta: {:#?}", delta);
+    println!("Writing delta: {:#?}", opts);
 
     // this directory will be created if it does not exist
     let path = "/tmp/metadata-tmp";
@@ -60,7 +101,7 @@ fn main() -> sled::Result<()> {
 
     let key = format!("deltas/{}", id);
 
-    let value = bincode::serialize(&delta).expect("serialize");
+    let value = bincode::serialize(&Delta::from((id, opts))).expect("serialize");
     db.insert(key, &IVec::from(value))?;
     Ok(())
 }
