ae966bb1f45523a85c5c6f2776ab1d8626be2134
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-29 13:53:20 -0500

    Draft

--- admin/create-diff-file-example.sh
+++ admin/create-diff-file-example.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-OLD_PACK="beta-0.0.0.pck"
-NEW_PACK="beta-0.0.1.pck"
-DIFF_OUT="beta-0.0.0.pck_to_beta.0.0.1.pck.diff"
+set -e
+
+
+USAGE="Must provide two arguments: /path/to/old.pck /path/to/new.pck"
+
+if [ -n "$1" ]; then
+    OLD_PACK=$1
+else
+    echo $USAGE
+    exit 1
+fi
+
+if [ -n "$2" ]; then
+    NEW_PACK=$2
+else
+    echo $USAGE
+    exit 1
+fi
+
+
+DIFF_OUT="${OLD_PACK}_to_${NEW_PACK}.diff"
 # size of the newer file, in bytes
 NEW_SIZE=`du -k "$NEW_PACK" | cut -f1`
 
 NUM_THREADS=8
-SORT_PARTITIONS="$(($NUM_THREADS - 1))"
-MAGIC="$(($NUM_THREADS * 3))"
+SORT_PARTITIONS=$((NUM_THREADS - 1))
+MAGIC=$((NUM_THREADS * 3))
 SCAN_CHUNK_SIZE_FIRST_PASS=$(( NEW_SIZE / MAGIC ))
 SCAN_CHUNK_SIZE=$(( SCAN_CHUNK_SIZE > 0 ? SCAN_CHUNK_SIZE : 1 ))
 
+
 echo "SORT_PARTITIONS $SORT_PARTITIONS"
 echo "SCAN_CHUNK_SIZE $SCAN_CHUNK_SIZE"
+echo "DIFF_OUT        $DIFF_OUT"
 
 bic diff $OLD_PACK $NEW_PACK $DIFF_OUT --method zstd --sort-partitions $SORT_PARTITIONS --scan-chunk-size $SCAN_CHUNK_SIZE
