ee2fd620427c683d19c18b162a60f430beb5c9a3
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-01 11:17:31 -0500

    rename project and godot refs

--- cross-compile.sh
+++ cross-compile.sh
@@ -2,5 +2,5 @@
 
 cargo build --target=x86_64-unknown-linux-gnu
 cargo build --target=x86_64-apple-darwin
-mv ./target/x86_64-apple-darwin/debug/libincremental_patch.dylib ./godot/.
-mv ./target/x86_64-unknown-linux-gnu/debug/libincremental_patch.so ./godot/.
+mv ./target/x86_64-apple-darwin/debug/libdeltapack.dylib ./godot/.
+mv ./target/x86_64-unknown-linux-gnu/debug/libdeltapack.so ./godot/.
