80e24666ee0bcbc60de6be953a05bf7edea60532
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-01 11:10:40 -0500

    fix script

--- cross-compile.sh
+++ cross-compile.sh
@@ -2,5 +2,5 @@
 
 cargo build --target=x86_64-unknown-linux-gnu
 cargo build --target=x86_64-apple-darwin
-mv ./target/x86_64-apple-darwin/debug/libincremental_patch.dylib ../game/.
-mv ./target/x86_64-unknown-linux-gnu/debug/libincremental_patch.so ../game/.
+mv ./target/x86_64-apple-darwin/debug/libincremental_patch.dylib ./godot/.
+mv ./target/x86_64-unknown-linux-gnu/debug/libincremental_patch.so ./godot/.
