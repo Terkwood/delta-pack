1e75d2dae103f2ed0255a7cf176814a0992a7b90
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-01 20:05:52 -0500

    add scoop script

---
+++ .github/workflows/install-clang.ps1
@@ -0,0 +1,11 @@
+# Check to see if we are currently running "as Administrator"
+# See: https://github.com/mvijfschaft/dotfiles/blob/master/install.ps1
+
+
+# https://github.com/lukesampson/scoop
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+
+scoop update
+
+scoop install clang
