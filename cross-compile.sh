87fb879469cb0f8b0d56056b71e487ff00ca394a
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-02 07:45:49 -0500

    alter cross-compile.sh to emit release bins

--- cross-compile.sh
+++ cross-compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cargo build --target=x86_64-unknown-linux-gnu
-cargo build --target=x86_64-apple-darwin
+cargo build --release --target=x86_64-unknown-linux-gnu
+cargo build --release --target=x86_64-apple-darwin
 mv ./target/x86_64-apple-darwin/debug/libdeltapack.dylib ./godot/.
 mv ./target/x86_64-unknown-linux-gnu/debug/libdeltapack.so ./godot/.
