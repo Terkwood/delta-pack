1abd1ecb2ce9eef6e2610990f2905035bec65776
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-02 07:47:42 -0500

    fix script

--- cross-compile.sh
+++ cross-compile.sh
@@ -2,5 +2,5 @@
 
 cargo build --release --target=x86_64-unknown-linux-gnu
 cargo build --release --target=x86_64-apple-darwin
-mv ./target/x86_64-apple-darwin/debug/libdeltapack.dylib ./godot/.
-mv ./target/x86_64-unknown-linux-gnu/debug/libdeltapack.so ./godot/.
+mv ./target/x86_64-apple-darwin/release/libdeltapack.dylib ./godot/.
+mv ./target/x86_64-unknown-linux-gnu/release/libdeltapack.so ./godot/.
